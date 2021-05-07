8d0109048b10a8afd714f0c85b233a8ecbb7129a
Jason Balaci <jasonbalaci@gmail.com>
2021-05-07 11:03:19 -0400

    trying to convert pbIsSafe without changing, but failing atm

--- code/drasil-example/Drasil/GlassBR/IMods.hs
+++ code/drasil-example/Drasil/GlassBR/IMods.hs
@@ -1,18 +1,24 @@
-module Drasil.GlassBR.IMods (symb, iMods, pbIsSafe, lrIsSafe, instModIntro) where
+module Drasil.GlassBR.IMods (symb, iMods, iMods0, iMods1, pbIsSafe, lrIsSafe, instModIntro) where
 
 import Prelude hiding (exp)
 import Language.Drasil
-import Theory.Drasil (InstanceModel, imNoDeriv, qwC, ModelKinds (OthModel))
+import Theory.Drasil (InstanceModel, imNoDeriv, qwC, ModelKinds (OthModel, EquationalModel))
 import Utils.Drasil
 
 import Drasil.GlassBR.DataDefs (probOfBreak, calofCapacity, calofDemand,
   pbTolUsr, qRef)
 import Drasil.GlassBR.Goals (willBreakGS)
 import Drasil.GlassBR.References (astm2009)
-import Drasil.GlassBR.Unitals (charWeight, demand, demandq, isSafeLR, isSafePb,
+import Drasil.GlassBR.Unitals (charWeight, demand, demandq, isSafeLR, isSafePb, isSafeProb,
   lRe, pbTol, plateLen, plateWidth, probBr, standOffDist)
 
 import Data.Drasil.Concepts.Documentation (goal)
+
+iMods0 :: [InstanceModel]
+iMods0 = [pbIsSafe]
+
+iMods1 :: [InstanceModel]
+iMods1 = [lrIsSafe]
 
 iMods :: [InstanceModel]
 iMods = [pbIsSafe, lrIsSafe]
@@ -24,16 +30,25 @@ symb = map dqdWr [plateLen, plateWidth, charWeight, standOffDist] ++
 {--}
 
 pbIsSafe :: InstanceModel
-pbIsSafe = imNoDeriv (OthModel pbIsSafeRC)
+pbIsSafe = imNoDeriv (EquationalModel pbIsSafeQD)
   [qwC probBr $ UpFrom (Exc, 0), qwC pbTol $ UpFrom (Exc, 0)]
   (qw isSafePb) []
   [makeCite astm2009] "isSafePb"
   [pbIsSafeDesc, probBRRef, pbTolUsr]
 
 
-pbIsSafeRC :: RelationConcept
-pbIsSafeRC = makeRC "safetyReqPb" (nounPhraseSP "Safety Req-Pb")
-  EmptyS (sy isSafePb $= sy probBr $< sy pbTol)
+pbIsSafeQD :: QDefinition
+pbIsSafeQD = mkQuantDef isSafePb pbIsSafeExpr
+
+-- fromEqn' "isSafePb" (nounPhraseSP "Safety Req-Pb") pbIsSafeDesc (eqSymb isSafePb) Boolean pbIsSafeExpr
+
+-- mkQuantDef' isSafePb (nounPhraseSP "Safety Req-Pb") pbIsSafeExpr
+
+pbIsSafeExpr :: Expr
+pbIsSafeExpr = sy probBr $< sy pbTol
+
+-- makeRC "safetyReqPb" (nounPhraseSP "Safety Req-Pb")
+--   EmptyS (sy isSafePb $= sy probBr $< sy pbTol)
 
 {--}
 
